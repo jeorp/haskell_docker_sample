5a71bb8e7523ab0226bb60db39e12a4efb6b8555
jeorp <jeorpqxx@gmail.com>
2021-09-11 17:07:52 +0900

    update

--- src/server/app/Main.hs
+++ src/server/app/Main.hs
@@ -3,26 +3,47 @@ module Main where
 import Data.Char
 import Network.HTTP
 import Text.HTML.TagSoup
+import Text.HTML.TagSoup.Match (tagOpen)
+
+data Broadcaster = BS String | TOKYO_MX String deriving Show
+
+data ProgramInfo = ProgramInfo {
+    broadcaster :: String,
+    title :: String,
+    category :: String,
+    description :: String,
+    url :: String, 
+    date :: String,
+    time :: String
+} deriving Show
 
 
 openURL :: String -> IO String
 openURL x = getResponseBody =<< simpleHTTP (getRequest x)
 
-haskellLastModifiedDateTime :: IO ()
-haskellLastModifiedDateTime = do
-    src <- openURL "http://wiki.haskell.org/Haskell"
-    let lastModifiedDateTime = fromFooter $ parseTags src
-    putStrLn $ "wiki.haskell.org was last modified on " ++ lastModifiedDateTime
-    where fromFooter = unwords . drop 6 . words . innerText . take 2 . dropWhile (~/= "<li id=lastmod>")
+bsParseCore :: [Tag String] -> [Tag String]
+bsParseCore html = do
+    let week_broadcaster = dropWhile (~/= tag_start_broadcaster)  html
+    week_broadcaster
+    where 
+        tag_start_broadcaster :: Tag String
+        tag_start_broadcaster = TagOpen "div" [("class", "broadcaster_box currentDate theday")]
+        tag_broadcaster :: Tag String
+        tag_broadcaster = TagOpen "div" [("class", "broadcaster_box")]
 
-bsParse :: [Tag String] -> String
-bsParse html = do
-    "parse bs program"
+bsSearchAnime :: String -> ProgramInfo 
+bsSearchAnime anime = do
+    ProgramInfo {broadcaster="", title="", category="", description="", url="", date="", time=""}
+
+bsListAllProgram :: [Tag String] -> [ProgramInfo]
+bsListAllProgram html = do
+    [ProgramInfo {broadcaster="", title="", category="", description="", url="", date="", time=""}]
+
 
 parse :: IO ()
 parse = do
     html <- readFile "BS11.html"
-    putStrLn $ bsParse $ parseTags html
+    writeFile "temp.txt" $ innerText $ bsParseCore $ parseTags html
     putStrLn "finish"
 
 main :: IO ()
