badf2d51605322cf24a74d6c14107dc2bd5747df
jeorp <jeorpqxx@gmail.com>
2021-09-11 13:48:17 +0900

    start haskell development

--- src/server/app/Main.hs
+++ src/server/app/Main.hs
@@ -1,6 +1,34 @@
 module Main where
 
-import Lib
+import Data.Char
+import Network.HTTP
+import Text.HTML.TagSoup
+
+
+openURL :: String -> IO String
+openURL x = getResponseBody =<< simpleHTTP (getRequest x)
+
+haskellLastModifiedDateTime :: IO ()
+haskellLastModifiedDateTime = do
+    src <- openURL "http://wiki.haskell.org/Haskell"
+    let lastModifiedDateTime = fromFooter $ parseTags src
+    putStrLn $ "wiki.haskell.org was last modified on " ++ lastModifiedDateTime
+    where fromFooter = unwords . drop 6 . words . innerText . take 2 . dropWhile (~/= "<li id=lastmod>")
+
+bsParse :: [Tag String] -> String
+bsParse html = do
+    "parse bs program"
+
+parse :: IO ()
+parse = do
+    html <- readFile "BS11.html"
+    putStrLn $ bsParse $ parseTags html
+    putStrLn "finish"
 
 main :: IO ()
-main = someFunc
+main = do
+    parse
+
+
+
+    
