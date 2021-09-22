a4eaaecadf49a8c82320e1e5c17d2798bd954dc3
jeorp <jeorpqxx@gmail.com>
2021-09-23 01:24:14 +0900

    update Main.hs

--- src/serverScotty/app/Main.hs
+++ src/serverScotty/app/Main.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import Text.Blaze.Html5.Attributes
+import Text.Blaze.Html.Renderer.Text
 import qualified Web.Scotty as S
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -10,5 +11,13 @@ import Data.Monoid (mconcat)
 
 main = S.scotty 3000 $
   S.get "/:word" $ do
-    beam <- S.param "word"
-    S.html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+    S.html $ renderHtml render
+
+-- html template by Blaze.Html5
+render = do
+  H.html $ do
+    H.body $ do
+      H.h1 "My todo list"
+      H.ul $ do
+        H.li "learn haskell"
+        H.li "make a website"
